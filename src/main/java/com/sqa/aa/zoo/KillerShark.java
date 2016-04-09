8d69e7b90190facb6750f69ae57dbe2c8b8fcc68
Anna Antonyan <=>
2016-04-09 12:30:21 -0700

    Adding KillerShark

---
+++ src/main/java/com/sqa/aa/zoo/KillerShark.java
@@ -0,0 +1,36 @@
+/**
+ *   File Name: Lion.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Apr 7, 2016
+ *
+ */
+
+package com.sqa.aa.zoo;
+
+/**
+ * Lion //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class KillerShark extends AbstractFish {
+
+	/**
+	 * @see com.sqa.aa.zoo.IAnimal#act()
+	 */
+	@Override
+	public void act() {
+		System.out.println("They can eat human");
+
+	}
+
+}
